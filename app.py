125106b1ce10fed3cdbfbf4d8af6a4012262aec9
MADHU SUDHAN REDDY <161938408+Madhusudhan1206@users.noreply.github.com>
2026-02-21 09:41:06 +0530

    Update page icon for ChurnSight app

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="ChurnSight | Customer Churn Prediction",
-    page_icon="business-model.png",
+    page_icon="business-model(1).png",
     layout="wide",
     initial_sidebar_state="expanded"
 )
